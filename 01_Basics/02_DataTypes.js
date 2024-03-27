983418912694b82ff94f26d82ff7dd8d25193be4
JSExcellence <165128775+JSExcellence@users.noreply.github.com>
2024-03-27 12:09:47 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //Treat all Js code as a newer version
+//alert(3 + 2); // We are using node Js not a browse so it not going to work here
+
+//Primitive Data Types
+
+let name = "Abc"; // String
+let age = 20; // Number
+let isLogedIn = false; // Boolean
+//null => Standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof ""); //string
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
